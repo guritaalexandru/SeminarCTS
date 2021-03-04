f1d48056f9be2bc7f1966493ea51601981f34504
Alexycys <a_gurita@yahoo.com>
2021-03-04 17:19:55 +0200

    Added abstract class instead of interface

--- Seminar_02_cts/src/ro/ase/cts/program/Program.java
+++ Seminar_02_cts/src/ro/ase/cts/program/Program.java
@@ -6,13 +6,18 @@ import java.util.List;
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.clase.readers.AngajatReader;
+import ro.ase.cts.clase.readers.AplicantReader;
 
 public class Program {
+	
+	public static List<Aplicant> citesteAplicanti(AplicantReader reader) throws FileNotFoundException{
+		return reader.readAplicanti();
+	}
 
 	public static void main(String[] args) {
 		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
+			listaAngajati = citesteAplicanti(new AngajatReader("angajati.txt"));
 			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
